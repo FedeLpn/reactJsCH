b6d3f3c33e6d1d0c198747f94128c790e7f17924
FedeLpn <fedelapine@gmail.com>
2022-05-20 00:58:20 -0300

    Navbar con estilos

---
+++ proyecto-lapine-ch/src/components/Navbar.js
@@ -0,0 +1,19 @@
+export const Navbar = () => {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+                <h1 className="header__logo">
+                    Bienvenidos a la nueva forma de comprar
+                </h1>
+
+                <nav className="header__navbar">
+                    <p className="header__navlink"> Ofertas del dia </p>
+                    <p className="header__navlink"> Productos </p>
+                    <p className="header__navlink"> Contacto </p>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+//export default Navbar
